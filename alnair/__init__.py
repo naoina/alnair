efb5dbd689fcd263ae6130e4686e23dc129bdffa
Naoya Inada <naoina@kuune.org>
2012-07-16 04:54:34 +0900

    Version bumped up to 0.3

--- alnair/__init__.py
+++ alnair/__init__.py
@@ -26,7 +26,7 @@
 
 
 __author__ = "Naoya Inada <naoina@kuune.org>"
-__version__ = '0.2'
+__version__ = '0.3'
 
 
 from alnair.package import (
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class pytest(TestCommand):
         import pytest
         raise SystemExit(pytest.main(self.test_args))
 
-version = '0.2'
+version = '0.3'
 
 install_requires = [
     'fabric>=1.4.2',
